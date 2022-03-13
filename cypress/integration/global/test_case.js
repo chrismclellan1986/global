b149b1c6ac3e697aee96cf6a3148e53d9ff850bb
Christopher McLellan <christopher.mclellan@bbc.co.uk>
2022-03-13 20:57:55 +0000

    Update test and read me

--- cypress/integration/global/test_case.js
+++ cypress/integration/global/test_case.js
@@ -1,15 +1,3 @@
-/** 
- * We’d like you to perform the same test we looked at during the interview, and would like to see what your solution would look like given more time and out of interview conditions.
-
-The main things we’re looking for is good structure and tests which aren’t too brittle. Aim for coverage of key things (the most useful tests),
-it doesn’t necessarily need to be fully comprehensive (carefully select what you can get done in the time).
-
-Please provide your solution in a Github repo (or your git hosting platform of choice).
-For Github please add the usernames byrion and chisnet to the project so we can view.
- * 
- * 
-*/
-
 import 'cypress-iframe'
 
 describe('Capital FM Now Playing Module', () => {
@@ -30,7 +18,7 @@ describe('Capital FM Now Playing Module', () => {
 
      it('Most Recent Songs display four songs', () => {
 
-        1/**
+        /**
           * Purpose of this test is to check that Most Recent songs displays the last four played songs.
           */
          cy.get('article.track')
@@ -43,16 +31,11 @@ describe('Capital FM Now Playing Module', () => {
          /**
           * The purpose of this test is to check that the href attribute is present and also that the link has either itunes.apple.com
           * or music.apple.com
-          * If either of them are not present the we state that the element  'a[class="itunes_download_lrg"' does not exist
+          * 
+          * Note: While testing I noticed that some songs didn't have either link 'music.apple.com' or ''itunes.apple.com'
+          * To further this test I would add a check to the conditional logic which would check if ''a[class="itunes_download_lrg"' existed
           */
-
-         cy.on('uncaughtException', (err, runnable) => {
-             expect(err.message).to.include('something about the error');
-             done();
-             return false;
-         })
-
-           
+            
          cy.get('a[class="itunes_download_lrg"').then(($a) => {
             if ($a.text().includes('music.apple.com')) {
                 cy.should('have.attr', 'href')
@@ -69,7 +52,7 @@ describe('Capital FM Now Playing Module', () => {
     it('See More link takes user to goes to last play song page', () => {
         /**
          * The purpose of this test is to check that the See More link takes user
-         *  to the Last Played Songs list page
+         * to the Last Played Songs list page
          */
 
         cy.get('a[class="see_more"').click();
